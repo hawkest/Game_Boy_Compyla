7cc4be4d11d6845ee889750c79bededad6d68c02
hawkest <benpyle83@gmail.com>
2025-08-02 23:21:31 +0100

    Update cpu.c
    fully implemented the $CB prefix instructions.
    
    next ldh [c], a

--- components/cpu.c
+++ components/cpu.c
@@ -2462,6 +2462,220 @@ void execute_prefix_instruction (uint8_t prefixed_opcode)
 		}
 		break;
 
+		case 0x20: case 0x21: case 0x22: case 0x23: case 0x24: case 0x25: case 0x26: case 0x27:
+		{
+			uint8_t opcode = prefixed_opcode;
+			uint8_t reg_code = opcode & 0x07;
+			uint16_t r16 = get_register_value(reg_code);
+			uint8_t bit7 = CHK_BIT(r16, 7);
+
+			if (bit7)
+			{
+				SET_BIT(cpu_regs.F, CPU_FLAG_CARRY_C_BIT);
+			}
+			else
+			{
+				CLR_BIT(cpu_regs.F, CPU_FLAG_CARRY_C_BIT);
+			}
+
+			r16 = (r16 << 1);
+
+			CLR_BIT(cpu_regs.F, CPU_FLAG_SUB_N_BIT);
+			CLR_BIT(cpu_regs.F, CPU_FLAG_HALF_H_BIT);
+
+			if (r16 == 0x00)
+			{
+				SET_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+			else
+			{
+				CLR_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+
+			set_register_value(reg_code, (uint8_t)(r16 & 0x00FF));
+		}
+		break;
+
+		case 0x28: case 0x29: case 0x2A: case 0x2B: case 0x2C: case 0x2D: case 0x2E: case 0x2F:
+		{
+			uint8_t opcode = prefixed_opcode;
+			uint8_t reg_code = opcode & 0x07;
+			uint8_t r8 = get_register_value(reg_code);
+			uint8_t bit0 = CHK_BIT(r8, 0);
+			uint8_t bit7 = CHK_BIT(r8, 7) << 7;
+
+
+			if (bit0)
+			{
+				SET_BIT(cpu_regs.F, CPU_FLAG_CARRY_C_BIT);
+			}
+			else
+			{
+				CLR_BIT(cpu_regs.F, CPU_FLAG_CARRY_C_BIT);
+			}
+
+			r8 = (r8 >> 1);
+
+			r8 = r8 | bit7;
+
+
+			CLR_BIT(cpu_regs.F, CPU_FLAG_SUB_N_BIT);
+			CLR_BIT(cpu_regs.F, CPU_FLAG_HALF_H_BIT);
+
+			if (r8 == 0x00)
+			{
+				SET_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+			else
+			{
+				CLR_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+
+			set_register_value(reg_code, r8);
+		}
+		break;
+
+		case 0x30: case 0x31: case 0x32: case 0x33: case 0x34: case 0x35: case 0x36: case 0x37:
+		{
+			uint8_t opcode = prefixed_opcode;
+			uint8_t reg_code = opcode & 0x07;
+			uint8_t r8 = get_register_value(reg_code);
+			uint8_t new_r8, new_MSN;
+
+			// takes the existing least sig nibble masks it moves it 4 to the left to make it the new MSN
+			new_MSN = (r8 & 0x0F) << 4;
+
+			// bit shift the current MSN and the bit wise ors the new MSN into new r8
+			new_r8 = (r8 >> 4) | new_MSN;
+
+			CLR_BIT(cpu_regs.F, CPU_FLAG_CARRY_C_BIT);
+			CLR_BIT(cpu_regs.F, CPU_FLAG_SUB_N_BIT);
+			CLR_BIT(cpu_regs.F, CPU_FLAG_HALF_H_BIT);
+
+			if (new_r8 == 0x00)
+			{
+				SET_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+			else
+			{
+				CLR_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+
+			set_register_value(reg_code, new_r8);
+		}
+		break;
+
+		case 0x38: case 0x39: case 0x3A: case 0x3B: case 0x3C: case 0x3D: case 0x3E: case 0x3F:
+		{
+			uint8_t opcode = prefixed_opcode;
+			uint8_t reg_code = opcode & 0x07;
+			uint8_t r8 = get_register_value(reg_code);
+			uint8_t bit0 = CHK_BIT(r8, 0);
+
+
+			if (bit0)
+			{
+				SET_BIT(cpu_regs.F, CPU_FLAG_CARRY_C_BIT);
+			}
+			else
+			{
+				CLR_BIT(cpu_regs.F, CPU_FLAG_CARRY_C_BIT);
+			}
+
+			r8 = (r8 >> 1);
+
+			CLR_BIT(r8, 7);
+
+
+			CLR_BIT(cpu_regs.F, CPU_FLAG_SUB_N_BIT);
+			CLR_BIT(cpu_regs.F, CPU_FLAG_HALF_H_BIT);
+
+			if (r8 == 0x00)
+			{
+				SET_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+			else
+			{
+				CLR_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+
+			set_register_value(reg_code, r8);
+		}
+		break;
+
+		case 0x40: case 0x41: case 0x42: case 0x43: case 0x44: case 0x45: case 0x46: case 0x47: // bit 0
+		case 0x48: case 0x49: case 0x4A: case 0x4B: case 0x4C: case 0x4D: case 0x4E: case 0x4F: // bit 1
+		case 0x50: case 0x51: case 0x52: case 0x53: case 0x54: case 0x55: case 0x56: case 0x57: // bit 2
+		case 0x58: case 0x59: case 0x5A: case 0x5B: case 0x5C: case 0x5D: case 0x5E: case 0x5F: // bit 3
+		case 0x60: case 0x61: case 0x62: case 0x63: case 0x64: case 0x65: case 0x66: case 0x67: // bit 4
+		case 0x68: case 0x69: case 0x6A: case 0x6B: case 0x6C: case 0x6D: case 0x6E: case 0x6F: // bit 5
+		case 0x70: case 0x71: case 0x72: case 0x73: case 0x74: case 0x75: case 0x76: case 0x77: // bit 6
+		case 0x78: case 0x79: case 0x7A: case 0x7B: case 0x7C: case 0x7D: case 0x7E: case 0x7F: // bit 7
+		{
+			uint8_t opcode = prefixed_opcode;
+			uint8_t reg_code = opcode & 0x07;
+			uint8_t bit_number = (opcode & 0x38) >> 3;
+			uint8_t r8 = get_register_value(reg_code);
+
+
+			CLR_BIT(cpu_regs.F, CPU_FLAG_SUB_N_BIT);
+			SET_BIT(cpu_regs.F, CPU_FLAG_HALF_H_BIT);
+
+			if (CHK_BIT(r8, bit_number))
+			{
+				CLR_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+			else
+			{
+				SET_BIT(cpu_regs.F, CPU_FLAG_ZERO_Z_BIT);
+			}
+
+		}
+		break;
+
+		case 0x80: case 0x81: case 0x82: case 0x83: case 0x84: case 0x85: case 0x86: case 0x87: // bit 0
+		case 0x88: case 0x89: case 0x8A: case 0x8B: case 0x8C: case 0x8D: case 0x8E: case 0x8F: // bit 1
+		case 0x90: case 0x91: case 0x92: case 0x93: case 0x94: case 0x95: case 0x96: case 0x97: // bit 2
+		case 0x98: case 0x99: case 0x9A: case 0x9B: case 0x9C: case 0x9D: case 0x9E: case 0x9F: // bit 3
+		case 0xA0: case 0xA1: case 0xA2: case 0xA3: case 0xA4: case 0xA5: case 0xA6: case 0xA7: // bit 4
+		case 0xA8: case 0xA9: case 0xAA: case 0xAB: case 0xAC: case 0xAD: case 0xAE: case 0xAF: // bit 5
+		case 0xB0: case 0xB1: case 0xB2: case 0xB3: case 0xB4: case 0xB5: case 0xB6: case 0xB7: // bit 6
+		case 0xB8: case 0xB9: case 0xBA: case 0xBB: case 0xBC: case 0xBD: case 0xBE: case 0xBF: // bit 7
+		{
+			uint8_t opcode = prefixed_opcode;
+			uint8_t reg_code = opcode & 0x07;
+			uint8_t bit_number = (opcode & 0x38) >> 3;
+			uint8_t r8 = get_register_value(reg_code);
+
+			CLR_BIT(r8, bit_number);
+
+			set_register_value(reg_code, r8);
+
+		}
+		break;
+
+		case 0xC0: case 0xC1: case 0xC2: case 0xC3: case 0xC4: case 0xC5: case 0xC6: case 0xC7: // bit 0
+		case 0xC8: case 0xC9: case 0xCA: case 0xCB: case 0xCC: case 0xCD: case 0xCE: case 0xCF: // bit 1
+		case 0xD0: case 0xD1: case 0xD2: case 0xD3: case 0xD4: case 0xD5: case 0xD6: case 0xD7: // bit 2
+		case 0xD8: case 0xD9: case 0xDA: case 0xDB: case 0xDC: case 0xDD: case 0xDE: case 0xDF: // bit 3
+		case 0xE0: case 0xE1: case 0xE2: case 0xE3: case 0xE4: case 0xE5: case 0xE6: case 0xE7: // bit 4
+		case 0xE8: case 0xE9: case 0xEA: case 0xEB: case 0xEC: case 0xED: case 0xEE: case 0xEF: // bit 5
+		case 0xF0: case 0xF1: case 0xF2: case 0xF3: case 0xF4: case 0xF5: case 0xF6: case 0xF7: // bit 6
+		case 0xF8: case 0xF9: case 0xFA: case 0xFB: case 0xFC: case 0xFD: case 0xFE: case 0xFF: // bit 7
+		{
+			uint8_t opcode = prefixed_opcode;
+			uint8_t reg_code = opcode & 0x07;
+			uint8_t bit_number = (opcode & 0x38) >> 3;
+			uint8_t r8 = get_register_value(reg_code);
+
+			SET_BIT(r8, bit_number);
+
+			set_register_value(reg_code, r8);
+
+		}
+		break;
+
+
 
 
 
